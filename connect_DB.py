46502c2fa1368bbdd16063814e4dc000fd406008
NikJaminsky <30468788+NikJaminsky@users.noreply.github.com>
2019-04-17 16:00:34 +0800

    Update connect_DB.py

--- connect_DB.py
+++ connect_DB.py
@@ -2,12 +2,14 @@ import psycopg2
 
 #Соединение с БД
 def connectToDB(request):#(request, db_file, db_user, db_password, localhost)
+	#Соеденить с БД
 	conn = psycopg2.connect(dbname='db_file', 
 							user='db_user', 
 							password='db_password', 
 							host='localhost')
 	cursor = conn.cursor()
 	
+	#Выполнить запрос
 	cursor.execute(request)#('''SELECT * 
 							#   FROM table''')
 	answer = cursor
@@ -15,16 +17,17 @@ def connectToDB(request):#(request, db_file, db_user, db_password, localhost)
 	conn.close()
 	return answer
 
+#Поиск в БД по ключевому слову
 def searchInDB(word):
 	#ключевое слово: запрос
+	#Пример 'расписание': 'SELECT что-то FROM timetable',
 	requests = {
 		'': '',
 		'': '',
 		'': '',
 		'': '',
-		'': '',
 		}
-	
+	#Выбрать запрос по ключевому слову
 	for key, value in requests:
 		if word == key:
 			request = value
